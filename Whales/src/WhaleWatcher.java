093bdfdd34ead8ae09788d614bdfe0f8293768e2
adamclarke888 <adamclarke88@gmail.com>
2018-12-07 15:09:46 +0000

    created whale objects, stored whales in an array

--- Whales/src/WhaleWatcher.java
+++ Whales/src/WhaleWatcher.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WhaleWatcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Whale right = new Whale("Right", "Atlantic", 2001, 21, 16);
+		Whale blue = new Whale("Blue", "Pacific", 2001, 23, 16);
+		Whale sperm = new Whale("Sperm", "Atlantic", 1900, 20, 40);
+		Whale humpback = new Whale("Humpback", "Antarctic", 919, 13, 13);
+		
+		// Use an array to store the four Whale objects
+		
+		ArrayList<Whale> whales = new ArrayList<Whale>();
+		
+		whales.add(right);
+		whales.add(blue);
+		whales.add(sperm);
+		whales.add(humpback);
+		
+		for(Whale whale:whales) { // for loop to show each whale in the whales arraylist
+			System.out.println(whale.getName() + " " + whale.getMainOcean() + " " + whale.getWeight() + " " + whale.getMaxSpeed() + " " + whale.getLength());
+		}
+		
 
 	}
 
