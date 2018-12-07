c68929ea530e2fe67e951830fb4cc66c41d42bb3
adamclarke888 <adamclarke88@gmail.com>
2018-12-07 14:50:54 +0000

    Created properties, constructors and getters and setters for whale class

--- Whales/src/Whale.java
+++ Whales/src/Whale.java
@@ -1,4 +1,49 @@
 
-public class Whale {
+public class Whale extends Animal {
+	
+	private String mainOcean;
+	private Integer weight;
+	private Integer maxSpeed;
+	private Integer length;
+	
+	public Whale(String name, String mainOcean, Integer weight, Integer maxSpeed, Integer length) {
+		super(name);
+		this.mainOcean = mainOcean;
+		this.weight = weight;
+		this.maxSpeed = maxSpeed;
+		this.length = length;
+	}
 
+	public String getMainOcean() {
+		return mainOcean;
+	}
+
+	public void setMainOcean(String mainOcean) {
+		this.mainOcean = mainOcean;
+	}
+
+	public Integer getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Integer weight) {
+		this.weight = weight;
+	}
+
+	public Integer getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(Integer maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public Integer getLength() {
+		return length;
+	}
+
+	public void setLength(Integer length) {
+		this.length = length;
+	}
+	
 }
